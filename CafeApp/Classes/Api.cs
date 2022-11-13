96b981034e9c24b7665a3ca3215ec305d8f4bf80
RodionPushkin <pushkin.rodion@yandex.ru>
2022-11-13 20:46:03 +0300

    Добавьте файлы проекта.

---
+++ CafeApp/Classes/Api.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CafeApp.Classes
+{
+    internal class Api
+    {
+        public void ReadOrder()
+        {
+
+        }
+        public void CreateOrder()
+        {
+
+        }
+
+        public void UpdateOrder()
+        {
+
+        }
+
+        public void DeleteOrder()
+        {
+
+        }
+
+        public void ReadTable()
+        {
+
+        }
+
+        public void CreateTable()
+        {
+
+        }
+
+        public void UpdateTable()
+        {
+
+        }
+
+        public void DeleteTable()
+        {
+
+        }
+
+        public void sfsdf()
+        {
+
+        }
+        public void Auth(string login,string password)
+        {
+
+        }
+
+        public void GetTables()
+        {
+           
+        }
+
+        public void GetCategories()
+        {
+
+        }
+
+        public void GetProductsByCategory( int id_category)
+        {
+
+        }
+
+        public void CreatePayment()
+        {
+
+        }
+    }
+}
---
+++ CafeApp/Classes/Navigation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace CafeApp.Classes
+{
+    public class Navigation
+    {
+        public static Frame CustomFrame { get; set; } = new Frame();
+
+        public static void OpenPage(Page page)
+        {
+            CustomFrame.Content = page;
+        }
+    }
+}
